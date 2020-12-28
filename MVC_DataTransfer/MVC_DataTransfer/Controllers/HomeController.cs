0d4447b62faeaf3514eddf4816c3c1ace6df397a
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2020-12-28 18:48:59 +0300

    MVCDataTransfer

---
+++ MVC_DataTransfer/MVC_DataTransfer/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DataTransfer.Models;
+
+namespace MVC_DataTransfer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //ViewBag.sunucutarihi = DateTime.Now;
+            //int vize=ViewBag.vize = 40;
+            //int final=ViewBag.final = 50;
+            //double ortalama =ViewBag.ortalama= vize * 0.4 + final * 0.6;
+            //ViewBag.devamsizlik = false;
+            //ViewBag.ogrencismi = "Mustafa Koca";
+            //ViewBag.odevnotlari= new int[] { 2,3,4,5};
+
+            ogrenci yeniogrenci = new ogrenci()
+            {
+                ogrenciadi = "Mustafa Koca",
+                adresi = "aydın",
+                bolumu = "bilgisayar",
+                sinifi = 3,
+                sunucutarihi = DateTime.Now,
+                vize=40,
+                final=50,
+                ortalama=46
+            };
+
+            return View(yeniogrenci);
+        }
+    }
+}
---
+++ MVC_DataTransfer/MVC_DataTransfer/Models/ogrenci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_DataTransfer.Models
+{
+    public class ogrenci
+    {
+        public  string ogrenciadi{ get; set; }
+        public string bolumu { get; set; }
+        public int sinifi { get; set; }
+        public string adresi { get; set; }
+        public DateTime sunucutarihi { get; set; }
+        public  int vize { get; set; }
+        public int final { get; set; }
+        public double ortalama { get; set; }
+    }
+}
